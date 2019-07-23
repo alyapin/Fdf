cb67d0ad09880ef40b3f6328dc85713ed0b55167
Kanata Zina <kzina@il-i5.21-school.ru>
2019-07-23 13:48:08 +0300

    first stage of project

---
+++ fdf.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kzina <kzina@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/19 16:41:55 by kzina             #+#    #+#             */
+/*   Updated: 2019/07/19 18:48:37 by kzina            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include "mlx.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include "libft/libft.h"
+
+struct          s_mlx
+{
+    void    *mlx;
+    void    *win;
+}typedef        t_mlx;
+
+struct          s_cord
+{
+    int             x;
+    int             y;
+    int             color;
+    int             z;
+    struct s_cord   *next;
+}typedef        t_cord;
---
+++ main.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kzina <kzina@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/18 15:24:18 by kzina             #+#    #+#             */
+/*   Updated: 2019/07/19 18:56:10 by kzina            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+
+/*int     deal(t_mlx *t)
+{
+    char s;
+    s = 'a';
+
+    mlx_pixels_put(t->mlx, t->win, 250, 250, 0xFFFFFF); 
+    return 0;
+}*/
+
+int     reader(int fd)
+{
+    char    *input;
+    char    *temp;
+    int     ct_str;
+    char    n;
+
+    ct_str = 0;
+    n = '\n';
+    input = (char*)ft_memalloc(sizeof(char));
+    while (get_next_line(fd, &temp) > 0)
+    {
+        input = ft_strjoin(input, temp);
+        input = ft_strjoin(input, &n);
+        ct_str++;
+    }
+    ft_putstr(input);
+    if (ct_str == 0)
+        return NULL;
+    return 0;
+}
+
+int     main(int ac, char **av)
+{   
+    void    *mlx_ptr;
+    void    *win_ptr;
+    t_mlx   *t;
+
+    if (ac != 2)
+    {
+        ft_putstr("usage: fdf input_file\n");
+        return (1);
+    }
+    if (reader(open(av[1], O_RDONLY)) == 1)
+    {
+        ft_putstr("reading from file error\n");
+        return (1);
+    }
+    /*mlx_ptr = mlx_init();
+    win_ptr = mlx_new_window(mlx_ptr, 500, 500, "First window");
+    t = (t_mlx*)malloc(sizeof(t_mlx));
+    t->mlx = mlx_ptr;
+    t->win = win_ptr;
+    mlx_pixel_put(mlx_ptr, win_ptr, 250, 250, 0xFFFFFF);
+    mlx_key_hook(win_ptr, deal, t);
+    mlx_loop(win_ptr);*/
+    return (0);
+}
