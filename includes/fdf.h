56f93c18961764f6dc973fcbeaef9d4d38a56558
Kanata Zina <kzina@il-f3.21-school.ru>
2019-07-27 15:30:56 +0300

    new structures for line algorithm

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,7 +6,7 @@
 /*   By: kzina <kzina@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/19 16:41:55 by kzina             #+#    #+#             */
-/*   Updated: 2019/07/26 18:11:39 by kzina            ###   ########.fr       */
+/*   Updated: 2019/07/27 14:45:35 by kzina            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,28 +17,49 @@
 # include <fcntl.h>
 # include "../libft/libft.h"
 
-struct          s_mlx
+typedef struct      s_mlx
 {
     void    *mlx;
     void    *win;
-}typedef        t_mlx;
+}                   t_mlx;
 
-struct          s_cord
+typedef struct      s_cord
 {
     int             x;
     int             y;
     int             color;
     int             z;
     struct s_cord   *next;
-}typedef        t_cord;
+}                   t_cord;
+typedef struct      s_line
+{
+    int             dx;
+    int             dy;
+    int             error;
+    int             k;
+    int             ystep;
+    int             y;
+    int             x;
+}                   t_line;
+
+typedef struct      s_linecor
+{
+    int             x0;
+    int             y0;
+    int             x1;
+    int             y1;
+}                   t_linecor;
+
+
 
 int     check(char *str);
 t_cord  *pars(char *str);
 int		ft_atoi_base(char *nb, int base);
 void    controls(t_mlx *param);
 t_mlx   *init_map(void);
-int			ft_atoi_base(char *nb, int base);
-t_cord      *new_t_cord(int x, int y, int z, int color);
-void        push_back_t_cord(t_cord **head, int x, int y, int z, int color);
+int		ft_atoi_base(char *nb, int base);
+t_cord  *new_t_cord(int x, int y, int z, int color);
+void    push_back_t_cord(t_cord **head, int x, int y, int z, int color);
+void    draw(t_linecor  *xy, t_mlx *param);
 
 #endif
